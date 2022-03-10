1e8fd5311de4085ea5a48b4a4a9cc9be1f42c204
Andrea Gussoni <85566597+andreagussoni@users.noreply.github.com>
2022-03-10 17:36:19 +0100

    bug fix

--- index.test.js
+++ index.test.js
@@ -2,11 +2,12 @@ const request = require("supertest");
 const app = require("./index.js");
 
 describe("Test the data path", () => {
-  test("It should response the GET method", () => {
-    return request(app)
-      .get("/data")
-      .then(response => {
-        expect(response.statusCode).toBe(200);
-      });
+    test("It should response the GET method", done => {
+      request(app)
+        .get("/")
+        .then(response => {
+          expect(response.statusCode).toBe(200);
+          done();
+        });
+    });
   });
-});
